7179fc3e4d2368110a128f77d167ab73031933d7
ngurahbas <ngurahbaskara@gmail.com>
2025-08-10 19:56:59 +0800

    initial commit

---
+++ src/main/kotlin/app/kanban/KanbanApplication.kt
@@ -0,0 +1,11 @@
+package app.kanban
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KanbanApplication
+
+fun main(args: Array<String>) {
+	runApplication<KanbanApplication>(*args)
+}
