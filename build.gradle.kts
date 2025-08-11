a85b1a816ffb96f2df702696bae489476011f8c7
ngurahbas <ngurahbaskara@gmail.com>
2025-08-11 11:50:51 +0800

    use gradle kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,106 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.node-gradle.node") version "7.0.1"
+}
+
+group = "app"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+    implementation ("org.springframework.boot:spring-boot-starter-actuator")
+	implementation ("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation ("org.springframework.boot:spring-boot-starter-web")
+	implementation ("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation ("org.jetbrains.kotlin:kotlin-reflect")
+	implementation ("org.springframework.session:spring-session-data-redis")
+	implementation ("org.flywaydb:flyway-core")
+	implementation ("org.flywaydb:flyway-database-postgresql")
+	developmentOnly ("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly ("org.postgresql:postgresql")
+	testImplementation ("org.springframework.boot:spring-boot-starter-test")
+	testImplementation ("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.named("processResources") {
+    dependsOn("npmCompileCss", "npmCompileJs")
+}
+
+node {
+    version.set("20.11.1")
+    npmVersion.set("10.2.4")
+    download.set(true)
+    nodeProjectDir.set(file("${project.projectDir}"))
+}
+
+/**
+ * initialize package.json
+ */
+tasks.register<com.github.gradle.node.npm.task.NpmTask>("npmInit") {
+    group = "npm"
+    description = "Initialize npm project with package.json"
+    dependsOn("npmSetup")
+
+    args.set(listOf("init", "-y"))
+    workingDir.set(file("${project.projectDir}"))
+}
+
+/**
+ * install or updates npm packages
+ */
+tasks.register<com.github.gradle.node.npm.task.NpmTask>("npmInstallPackages") {
+    group = "npm"
+    description = "Install npm packages"
+    dependsOn("npmSetup")
+
+    args.set(listOf("install", "--save-dev", "tailwindcss", "@tailwindcss/cli", "daisyui", "alpinejs", "esbuild", "htmx.org"))
+    workingDir.set(file("${project.projectDir}"))
+}
+
+/**
+ * compile CSS
+ */
+tasks.register<com.github.gradle.node.npm.task.NpmTask>("npmCompileCss") {
+    group = "npm"
+    description = "Compile CSS"
+    dependsOn("npmInstallPackages")
+
+    args.set(listOf("exec", "npx", "@tailwindcss/cli", "--", "-i", "src/main/css/main.css", "-o", "src/main/resources/static/css/main.css"))
+    workingDir.set(file("${project.projectDir}"))
+}
+
+/**
+ * compile js
+ */
+tasks.register<com.github.gradle.node.npm.task.NpmTask>("npmCompileJs") {
+    group = "npm"
+    description = "Compile JS"
+    dependsOn("npmInstallPackages")
+
+    args.set(listOf("exec", "npx", "esbuild", "--", "src/main/js/main.js", "--bundle", "--outfile=src/main/resources/static/js/main.js"))
+    workingDir.set(file("${project.projectDir}"))
+}
