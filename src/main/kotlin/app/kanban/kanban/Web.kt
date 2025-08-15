7aa819c78d26a11e1765df2c256f91763cf1f03e
ngurahbas <ngurahbaskara@gmail.com>
2025-08-15 12:51:13 +0800

    edit title flow

--- src/main/kotlin/app/kanban/kanban/Web.kt
+++ src/main/kotlin/app/kanban/kanban/Web.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 
 
@@ -15,7 +16,7 @@ class KanbanController(
     @GetMapping("/kanban")
     fun board(model: Model): String {
         model.addAttribute("editKanbanTitle", true)
-        model.addAttribute("kanban", KanbanWeb(null, "", listOf()))
+        model.addAttribute("kanban", KanbanWeb(null, "", defaultColumns ))
         return "kanban"
     }
 
@@ -28,10 +29,21 @@ class KanbanController(
         response.addHeader("HX-PUSH", "/kanban/${returnedId}")
         return "kanban :: kanbanTitle"
     }
+
+    @GetMapping("/kanban/{id}")
+    fun board(@PathVariable id: Long, model: Model): String {
+        val kanbanDb = service.getBoard(id)
+        model.addAttribute("editKanbanTitle", false)
+        model.addAttribute("kanban", KanbanWeb(kanbanDb.id, kanbanDb.title, kanbanDb.columns))
+        return "kanban"
+    }
 }
+
+val defaultColumns = listOf("To do", "In progress", "Done")
 
 data class KanbanWeb(
     val id: Long?,
     val title: String,
-    val columns: List<String>?
+    val columns: List<String>?,
+    val editKanbanTitle: Boolean = false
 )
