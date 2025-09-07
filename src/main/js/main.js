62c17dd9ff54da9d72644901d94ad5dfcfcac88c
ngurahbas <ngurahbaskara@gmail.com>
2025-09-07 21:11:53 +0800

    remove alpine

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,12 +1,8 @@
 import htmx from 'htmx.org';
-import Alpine from 'alpinejs';
 
 window.htmx = htmx;
-window.Alpine = Alpine;
 
 htmx.config.globalViewTransitions = true;
-
-Alpine.start();
 
 document.body.addEventListener("htmx:afterSwap", function(evt) {
     if (evt.detail.target) {
