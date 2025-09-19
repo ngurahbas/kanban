fc984b607724753232e2711bf7dd9ebb8044c58b
ngurahbas <ngurahbaskara@gmail.com>
2025-09-19 12:03:50 +0800

    integrate Alpine.js and enable its initialization alongside htmx

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,11 +1,19 @@
 import htmx from 'htmx.org';
+import Alpine from 'alpinejs'
 
 window.htmx = htmx;
+window.Alpine = Alpine;
 
 htmx.config.globalViewTransitions = true;
 
+Alpine.start();
+
 document.body.addEventListener("htmx:afterSwap", function(evt) {
-    if (evt.detail.target) {
+    if (evt.detail && evt.detail.target) {
         htmx.process(evt.detail.target);
+        try {
+            Alpine.initTree(evt.detail.target);
+        } catch (_) {
+        }
     }
 });
