4373026a36c935d512792ad5d99cb5915e86adc2
ngurahbas <ngurahbaskara@gmail.com>
2025-10-31 12:26:54 +0800

    ensure test database being used.

--- src/test/kotlin/app/kanban/KanbanApplicationTests.kt
+++ src/test/kotlin/app/kanban/KanbanApplicationTests.kt
@@ -3,16 +3,28 @@ package app.kanban
 import app.kanban.test.PostgresContainerConfiguration
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import kotlin.test.assertEquals
 
 @ExtendWith(SpringExtension::class)
 @ExtendWith(PostgresContainerConfiguration::class)
 @SpringBootTest
 class KanbanApplicationTests {
 
-	@Test
+    @Autowired
+    private lateinit var jdbcTemplate: JdbcTemplate
+
+    @Test
 	fun contextLoads() {
 	}
+    
+    @Test
+    fun `is using test container`() {
+        val dbName = jdbcTemplate.queryForObject("SELECT current_database()", String::class.java)
+        assertEquals("kanban_test", dbName)
+    }
 
 }
