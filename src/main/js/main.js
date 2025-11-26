da8ae70bd0b4e45d2b6b9b347e9b1d1aeafe98a7
ngurahbas <ngurahbaskara@gmail.com>
2025-11-26 13:00:12 +0800

    remove htmx:afterSwap event listener and related Alpine initialization logic

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,21 +1,9 @@
 import htmx from 'htmx.org';
 import Alpine from 'alpinejs';
 
-
 window.htmx = htmx;
 window.Alpine = Alpine;
 
 htmx.config.globalViewTransitions = true;
 
 Alpine.start();
-
-document.body.addEventListener("htmx:afterSwap", function(evt) {
-    if (evt.detail && evt.detail.target) {
-        htmx.process(evt.detail.target);
-        try {
-            Alpine.initTree(evt.detail.target);
-        } catch (e) {
-            console.error("Error initializing Alpine tree after htmx swap.", e);
-        }
-    }
-});
